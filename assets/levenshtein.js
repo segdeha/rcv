5cd4e01bf2a8b6552f3ffaef9717378ee5844269
Andrew Hedges <andrew@assistivlabs.com>
2025-06-25 15:45:01 -0700

    OK srsly stop futzing with Levenshtein

--- assets/levenshtein.js
+++ assets/levenshtein.js
@@ -6,19 +6,20 @@ class Levenshtein {
   }
 
   // Return the smallest of the three numbers passed in
-  minit(x, y, z) {
+  min3(x, y, z) {
     if (x <= y && x <= z) return x;
     if (y <= x && y <= z) return y;
     return z;
   }
 
-  // Calculate the normalized (distance / length of the longer string) distance
-  // between two strings
+  // Calculate the distance between two strings normalized as:
+  // distance / length of the longer string
   calc(a, b) {
     let m = a.length;
     let n = b.length;
 
-    // Make sure a.length >= b.length to use O(min(n,m)) space, whatever that is
+    // Make sure a.length >= b.length to use O(min(m,n)) space, whatever that is
+    // IOW, swap ’em around if m is shorter than n
     if (m < n) {
       [b, a] = [a, b];
       [n, m] = [m, n];
@@ -33,15 +34,16 @@ class Levenshtein {
       r[i] = []; r[i][0] = i;
       for (let j = 1; j < n + 1; ++j ) {
         const cost = a.charAt(i - 1) === b.charAt(j - 1) ? 0 : 1;
-        r[i][j] = this.minit(r[i-1][j] + 1, r[i][j-1] + 1, r[i-1][j-1] + cost);
+        r[i][j] = this.min3(
+          r[i-1][j] + 1,
+          r[i][j-1] + 1,
+          r[i-1][j-1] + cost
+        );
       }
     }
 
     // Normalize based on the length of the longer string
-    let distance = r[r.length - 1][r[r.length - 1].length - 1];
-    distance /= m > n ? m : n;
-
-    return distance;
+    return r[r.length - 1][r[r.length - 1].length - 1] / Math.max(m, n);
   }
 }
 
